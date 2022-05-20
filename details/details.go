e563c624f66ecc6afc22930e7f35ed1a42115a9a
fahmiaz411 <fahmiaziz884@gmail.com>
2022-05-20 09:41:29 +0700

    get ip

---
+++ details/details.go
@@ -0,0 +1,18 @@
+package details
+
+import (
+	"log"
+	"net"
+)
+
+func GetIP() (net.IP, error) {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP, nil
+}
